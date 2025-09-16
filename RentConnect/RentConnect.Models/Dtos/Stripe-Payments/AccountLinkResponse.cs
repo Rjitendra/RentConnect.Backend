84c2c373ea9abd1f2dccc201fe985308111d3308
Jitendra Behera <jb.jitu@live.com>
2025-09-16 20:27:24 +0530

    vv

---
+++ RentConnect/RentConnect.Models/Dtos/Stripe-Payments/AccountLinkResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentConnect.Models.Dtos.Stripe_Payments
+{
+    public class AccountLinkResponse
+    {
+        public string Object { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public string Url { get; set; }
+    }
+}
